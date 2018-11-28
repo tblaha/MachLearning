fe27816bda42c929e860ffc66f44f89461d53bc6
Unknown <andrija.drk1911@gmail.com>
2018-11-28 22:51:19 +0100

    hierarchical clustering

--- Imports/importdata_2018.m
+++ Imports/importdata_2018.m
@@ -1,8 +1,10 @@
 if(version()==('9.5.0.944444 (R2018b)'))
-    data=load('XoneoutofK.mat');
-    %data=load('X.mat');
+    %data=load('XoneoutofK.mat');
+    data=load('X.mat');
     X=data.X;
+    datann=load('Xnn.mat'); % not normalizes values
+    Xnn=datann.Xnn;
 else
-    run(importdata_Report2.m); %For K out of N
-    %run(importdata_Report1.m); %For K
+    %run(importdata_Report2.m); %For K out of N
+    run(importdata_Report1.m); %For K
 end
