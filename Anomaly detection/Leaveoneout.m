f293dec0850caadf5dbed8451d7f6b75f40654d7
Unknown <s171834@student.dtu.dk>
2018-12-03 16:13:38 +0100

    Outlier stuff

--- Anomaly detection/Leaveoneout.m
+++ Anomaly detection/Leaveoneout.m
@@ -3,21 +3,20 @@ addpath(genpath('../'))
 warning('off', 'all')
 
 clear
-%%
-out = fitgmdist(X,4,'Options')
 %% Configuration
 
 % import data
 %importdata_Report1 % non-one-out-of-k-coded
-if(version()==('9.5.0.944444 (R2018b)'))
+%if(version()==('9.5.0.944444 (R2018b)'))
     %data=load('../XoneoutofK.mat');
     data=load('X.mat');
     X=data.X;
-else
+%else
     %importdata_Report2; %For K out of N
-    importdata_Report1; %For K
-end
+%    importdata_Report1; %For K
+%end
 %L = @(y,yM) bayesloss(y,yM);
+%out = fitgmdist(X,4,'Options')
 %% No feature selection, just use all.
 % But do some numerical analysis to figure out a suitable tree size
 
@@ -66,7 +65,7 @@ features = features_avail( ~ismember(features_avail, outarg) );
 %w = 5;
 
 % Estimate optimal kernel density width by leave-one-out cross-validation
-widths=max(var(X))*(2.^[-10:2]);
+widths=max(var(X))*(2.^[-10:10]);
 for w=1:length(widths)
     [f, log_f] = gausKernelDensity(X, widths(w));
     logP(w)=sum(log_f);
@@ -87,10 +86,28 @@ f = gausKernelDensity(X, width);
 disp(i(1));
 
 % Plot density estimate outlier scores
-%figure('Gaussian Kernel Density: outlier score'); clf;
+mfig('Gaussian Kernel Density: outlier score'); clf;
 bar(y(1:20));
+xlabel('Index')
+title('Gaussian Kernel Density: outlier score')
+set(gca,'XTick',1:20,'XTickLabel',i,'FontSize',14)
+% mfig( Possible outliers'); clf;
+mfig('Gaussian Kernel Density: Plot')
+plot(y(1:100));
+%%
+clusterplot(X,y,zeros(392,1))
+%%
+x=[1 1 2 2 3 3 4 4];
+y=[2 3 1 2 3 4 3 2];
 
-%mfig('Gaussian Kernel Density: Possible outliers'); clf;
+figure(1)
+scatter(x,y)
+
+figure(2)
+axis([min(x)-1, max(x)+1, min(y)-1, max(y)+1])
+for k=1:length(x)
+    text(x(k),y(k),num2str(k))
+end
 %%
 Px = [...
 -0.3987    0.0631    0.2064   -0.1998    0.4816   -0.6655    0.2280   -0.1664
@@ -103,26 +120,6 @@ Px = [...
     0.2710    0.5181   -0.2842   -0.7485   -0.0865   -0.0406   -0.0548   -0.0702];
 
 PCAs = X*Px(:,[1,2]);
-%%
-%% Reference plots
-
-figCyl = figure('Position', [100 100 800 500], 'Visible', 'off');
-hold on
-for k = 1:2
-    scatter(scat(ismember(cyls,cylgroups{k}),1),...
-            scat(ismember(cyls,cylgroups{k}),2),... 'b',...
-            'DisplayName', strcat('Cylinder: ', mat2st  r(cylgroups{k}')))
-end
-hold off
-title('Scatter Plot for the raw data -- Number of Cylinders', 'FontSize', 16)
-xlabel('PCA1 score', 'FontSize', 16)
-ylabel('PCA2 score', 'FontSize', 16)
-grid on
-axis equal
-hold off
-legend({}, 'Location', 'SouthWest', 'Fontsize', 14)
-saveas(figCyl, 'Plots/ScatCyl.eps', 'epsc')
-
 %%
 scat=PCAs;
 figOri = figure('Position', [100 100 800 500], 'Visible', 'off');
